eca076bd7d5605129767efc6c30afad15883d1a4
Matthew Gordon <matt.gordon@crowdflower.com>
2012-10-09 02:37:24 -0700

    Add last to available criteria

--- spec/dynamoid/criteria_spec.rb
+++ spec/dynamoid/criteria_spec.rb
@@ -1,22 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "Dynamoid::Criteria" do
-  
   before do
     @user1 = User.create(:name => 'Josh', :email => 'josh@joshsymonds.com')
     @user2 = User.create(:name => 'Justin', :email => 'justin@joshsymonds.com')
+    @user3 = User.create(:name => 'Josh', :email => 'josh_0@joshsymonds.com')
   end
-  
+
   it 'finds first using where' do
     User.where(:name => 'Josh').first.should == @user1
   end
-  
+
+  it "finds last using where" do
+    User.where(:name => 'Josh').last.should == @user3
+    User.where(:name => 'Justin').last.should == @user2
+  end
+
   it 'finds all using where' do
-    User.where(:name => 'Josh').all.should == [@user1]
+    User.where(:name => 'Josh').all.should == [@user1, @user3]
   end
   
   it 'returns all records' do
-    User.all.should =~ [@user1, @user2]
+    User.all.should =~ [@user1, @user2, @user3]
     User.all.first.new_record.should be_false
   end
   
@@ -30,14 +35,14 @@ describe "Dynamoid::Criteria" do
   
   it 'passes each to all members' do
     User.each do |u|
-      u.id.should == @user1.id || @user2.id
+      u.id.should == @user1.id || @user2.id || @user3.id
       u.new_record.should be_false
     end
   end
 
   it 'returns n records' do
     User.limit(1).size.should eq(1)
-    5.times { |i| User.create(:name => 'Josh', :email => 'josh_#{i}@joshsymonds.com') }
+    4.times { |i| User.create(:name => 'Josh', :email => 'josh_#{i}@joshsymonds.com') }
     User.where(:name => 'Josh').all.size.should == 6
     User.where(:name => 'Josh').limit(2).size.should == 2
   end
@@ -68,5 +73,4 @@ describe "Dynamoid::Criteria" do
       User.where(:password => 'password').consistent.first
     end.to raise_error(Dynamoid::Errors::InvalidQuery)
   end
-    
 end
