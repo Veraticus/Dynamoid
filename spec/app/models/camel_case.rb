964dda7b993aac91f73e97dd86e0993583642432
Jorge <jorge@naturebox.com>
2015-05-28 13:29:45 -0700

    run :save callbacks on update and update!

--- spec/app/models/camel_case.rb
+++ spec/app/models/camel_case.rb
@@ -7,18 +7,20 @@ class CamelCase
   has_many :users
   has_one :sponsor
   has_and_belongs_to_many :subscriptions
-  
+
   before_create :doing_before_create
   after_create :doing_after_create
   before_update :doing_before_update
   after_update :doing_after_update
-  
+  before_save :doing_before_save
+  after_save :doing_after_save
+
   private
-  
+
   def doing_before_create
     true
   end
-  
+
   def doing_after_create
     true
   end
@@ -28,6 +30,14 @@ class CamelCase
   end
 
   def doing_after_update
+    true
+  end
+
+  def doing_before_save
+    true
+  end
+
+  def doing_after_save
     true
   end
 
